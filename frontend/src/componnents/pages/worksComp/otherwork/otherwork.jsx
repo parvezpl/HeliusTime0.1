e565e7d067448a2b5a59745fc400217c7412029a
ParvezAlam <parvezalam336@gmail.com>
2024-10-15 12:47:15 +0530

    problem fix1

--- frontend/src/componnents/pages/worksComp/otherwork/otherwork.jsx
+++ frontend/src/componnents/pages/worksComp/otherwork/otherwork.jsx
@@ -6,31 +6,40 @@ export function Otherwork() {
   const [userData, setUserData] = useState([])
 
   const getData = async () => {
-    try {
-      await axios.get("/api")
-        .then((res) => {
-          console.log(res.data)
-          setUserData(res.data)
-        })
-    } catch (error) {
-      console.log("catch erro")
-    }
+    // try {
+    //   await axios.get("/api/getuser")
+    //     .then((res) => {
+    //       console.log(res.data)
+    //       setUserData(res.data)
+    //     })
+    // } catch (error) {
+    //   console.log("catch erro")
+    // }
   }
 
   useEffect(() => {
     getData()
   }, [])
   //  console.log(userData)
+
+  const clickhandle = async ()=>{
+    await axios.get("/api/getuser")
+    .then((res)=>{
+      console.log(res.data)
+    })
+    console.log("hell")
+  }
   return (
     <div >
-      {
+      <button onClick={clickhandle}>hello</button>
+      {/* {
         userData?.map((item, index) =>
           <div key={index} style={{ "display": "flex", "margin": "10px", gap: "300px" }}>
             <p>{item.name}</p>
             <p>{item._id}</p>
           </div>
         )
-      }
+      } */}
     </div>
   )
 }
