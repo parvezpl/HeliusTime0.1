fa98e9a0460b86cb9ebd51750ad00e6534645f29
ParvezAlam <parvezalam336@gmail.com>
2024-10-15 13:46:23 +0530

    fix4

--- frontend/src/componnents/pages/worksComp/otherwork/apidata.js
+++ frontend/src/componnents/pages/worksComp/otherwork/apidata.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-// const API_URL = 'http://localhost:5000/api'; // Replace with your backend URL
+const API_URL = 'https://heliustimebackend.onrender.com'; // Replace with your backend URL
 
 export const fetchData = async () => {
   try {
-    const response = await axios.get(`/api`); // Replace with your endpoint
+    const response = await axios.get(`${API_URL}/api`); // Replace with your endpoint
     console.log("fetchdata", response)
     return response.data;
   } catch (error) {
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // // '/api':"https://heliustimebackend.onrender.com"
 // https://vitejs.dev/config/ 
 export default defineConfig({
-  server: {
-    proxy:{
-      // '/api':"http://localhost:5000"
-      // '/api':"http://localhost:3000"
-      '/api':"https://heliustimebackend.onrender.com"
+  // server: {
+  //   proxy:{
+  //     // '/api':"http://localhost:5000"
+  //     // '/api':"http://localhost:3000"
+  //     '/api':"https://heliustimebackend.onrender.com"
      
-    }
+  //   }
 
-  },
+  // },
   plugins: [react()],
 })
